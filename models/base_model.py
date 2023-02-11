0c1af3f3ca1192dcd13b15980cde19f8f7c10ad9
root <foogundipe@student.oauife.edu.ng>
2023-02-10 17:22:51 -0800

    fix some bugs

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.save()
+            models.storage.new(self)
 
         def __str__(self):
             '''
