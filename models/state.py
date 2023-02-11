112729823e678e9a54aa4a34bad09c8836c72310
root <foogundipe@student.oauife.edu.ng>
2023-02-10 18:29:00 -0800

    add state and city classes

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+''' A State class that inherits from BaseModel '''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+    Public class attribute
+
+    name: string - empty string
+    '''
+    name = ""
